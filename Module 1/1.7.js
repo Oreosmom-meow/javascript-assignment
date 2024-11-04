a555cc7a72aecc73f522cb974460cc5b418fdd66
Yutong Deng <yutong.deng09@gmail.com>
2024-11-04 11:31:12 +0200

    add 1.7

---
+++ Module 1/1.7.js
@@ -0,0 +1,12 @@
+function    rollDiceandCalculate(number)
+{
+    let result = 0;;
+    for (let i = 0; i < number; i++)
+    {
+       result += Math.floor(Math.random() * 6) + 1;
+       console.log(result);
+    }
+    return (result);
+}
+let user_input = parseInt(prompt('Please input a number'))
+document.querySelector('#target').innerHTML = 'With ' + user_input + ' you got ' + rollDiceandCalculate(user_input);
