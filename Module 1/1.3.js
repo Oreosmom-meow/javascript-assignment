0d6283fa14402be7e125407f7d06d29fa432f231
Yutong Deng <yutong.deng09@gmail.com>
2024-11-04 10:31:36 +0200

    add 1.1, 1.2, 1.3

---
+++ Module 1/1.3.js
@@ -0,0 +1,8 @@
+'use strict';
+let number1 = parseInt(prompt('Type number 1.'));
+let number2 = parseInt(prompt('Type number 2.'));
+let number3 = parseInt(prompt('Type number 3.'));
+let sum = number1 + number2 + number3;
+let product = number1 * number2 * number3;
+let average = sum / 3;
+document.querySelector('#target').innerHTML = 'sum = ' + sum  + '<br>product = ' + product + '<br>average= ' + average;
