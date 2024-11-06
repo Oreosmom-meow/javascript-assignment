8d50bc91f96c1a265da05124eb28992f74309787
Yutong Deng <yutong.deng09@gmail.com>
2024-11-06 11:46:14 +0200

    finish 2.4

---
+++ Module 2/2.4.js
@@ -0,0 +1,28 @@
+'user strict';
+function compareNumbers(a,b)
+{
+    return b - a;
+}
+function userInput()
+{
+    const numberArray = [];
+    let inputNumber = parseInt(prompt('Input a number'));
+    while (inputNumber != 0)
+    {
+        numberArray.push(inputNumber);
+        inputNumber = parseInt(prompt('Input a number'));
+    }
+    if (inputNumber == 0)
+    {
+        numberArray.push(inputNumber);
+        return (numberArray);
+    }
+}
+
+function sortArray(array)
+{
+    array.sort(compareNumbers);
+    console.log(array);
+}
+
+sortArray(userInput());
